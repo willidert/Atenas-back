8ff39c8ea9ab0ee68063c93b1b180293690afb75
William Teixeira <williderteixeira@gmail.com>
2021-11-27 14:06:51 -0400

    starting the project

---
+++ app/Models/Person.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    use HasFactory;
+
+    protected $table = 'persons';
+
+    public function categories()
+    {
+        return $this->hasOne(Category::class, 'id', 'category_id');
+    }
+}
